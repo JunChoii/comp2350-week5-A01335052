231fb3e6654326685559f8f034f3153fb49159cc
JunChoi <97496821+JunChoii@users.noreply.github.com>
2023-02-08 23:30:20 -0800

    Add files via upload

--- databaseAccessLayer.js
+++ databaseAccessLayer.js
@@ -1,21 +1,18 @@
-const database = include('/databaseConnection');
-
+const database = include("/databaseConnection");
 
 async function getAllUsers() {
-	let sqlQuery = `
-        SELECT web_user_id, first_name, last_name, email 
-        FROM freedb_comp2350-week4-A01335052.web_user`;
-	
-	try {
-		const results = await database.query(sqlQuery);
-		console.log(results[0]);
-		return results[0];
-	}
-	catch (err) {
-		console.log("Error selecting from todo table");
-		console.log(err);
-		return null;
-	}
+  let sqlQuery = `SELECT web_user_id, first_name, last_name, email
+	FROM freedb_comp2350-week4-A01335052.web_user`;
+
+  try {
+    const results = await database.query(sqlQuery);
+    console.log(results[0]);
+    return results[0];
+  } catch (err) {
+    console.log("Error selecting from todo table");
+    console.log(err);
+    return null;
+  }
 }
 
-module.exports = {getAllUsers}
+module.exports = { getAllUsers };
